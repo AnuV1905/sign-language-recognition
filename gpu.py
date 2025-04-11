9896cc4aee89b5d2e328fe49acaa73c0dc9dabed
Anushka Verma <vermaanushka1905@gmail.com>
2025-04-12 00:08:55 +0530

    gpu updation

--- gpu.py
+++ gpu.py
@@ -1,7 +1,5 @@
 import tensorflow as tf
 
-gpus = tf.config.list_physical_devices('GPU')
-if gpus:
-    print(f"✅ GPU available: {gpus}")
-else:
-    print("❌ No GPU detected by TensorFlow")
+print("TensorFlow version:", tf.__version__)
+print("Num GPUs Available:", len(tf.config.list_physical_devices('GPU')))
+print("GPU Devices:", tf.config.list_physical_devices('GPU'))
