39c2cf589b8610ec789eacd99ea5f0e08365ac0a
Anushka Verma <130084364+AnuV1905@users.noreply.github.com>
2025-04-13 07:25:40 +0000

    added cv testing in gpu.py

--- gpu.py
+++ gpu.py
@@ -1,5 +1,18 @@
-import tensorflow as tf
+# GPU testng section
+# import tensorflow as tf
 
-print("TensorFlow version:", tf.__version__)
-print("Num GPUs Available:", len(tf.config.list_physical_devices('GPU')))
-print("GPU Devices:", tf.config.list_physical_devices('GPU'))
+# print("TensorFlow version:", tf.__version__)
+# print("Num GPUs Available:", len(tf.config.list_physical_devices('GPU')))
+# print("GPU Devices:", tf.config.list_physical_devices('GPU'))
+# #################################
+
+# cv testing section
+import cv2
+
+cap = cv2.VideoCapture(0)
+if cap.isOpened():
+    print("Webcam accessed successfully!")
+else:
+    print("Failed to access webcam.")
+cap.release()
+
