6dc6ada588430a4dd4f2795c3bfdd225174e6a37
Anushka Verma <vermaanushka1905@gmail.com>
2025-04-21 21:17:39 +0530

    changes in app.py

--- app.py
+++ app.py
@@ -118,29 +118,35 @@ class Application:
         self.current_image = None
         self.current_image2 = None
         
-        self.json_file = open(os.path.join(self.directory, "model-bw.json"), "r")
-        self.model_json = self.json_file.read()
-        self.json_file.close()
-        self.loaded_model = model_from_json(self.model_json)
-        self.loaded_model.load_weights(os.path.join(self.directory, "model-bw.h5"))
+        # self.json_file = open(os.path.join(self.directory, "model-bw.json"), "r")
+        # self.model_json = self.json_file.read()
+        # self.json_file.close()
+        # self.loaded_model = model_from_json(self.model_json)
+        # self.loaded_model.load_weights(os.path.join(self.directory, "model-bw.h5"))
 
-        self.json_file_dru = open(os.path.join(self.directory, "model-bw_dru.json"), "r")
-        self.model_json_dru = self.json_file_dru.read()
-        self.json_file_dru.close()
-        self.loaded_model_dru = model_from_json(self.model_json_dru)
-        self.loaded_model_dru.load_weights(os.path.join(self.directory, "model-bw_dru.h5"))
+        # self.json_file_dru = open(os.path.join(self.directory, "model-bw_dru.json"), "r")
+        # self.model_json_dru = self.json_file_dru.read()
+        # self.json_file_dru.close()
+        # self.loaded_model_dru = model_from_json(self.model_json_dru)
+        # self.loaded_model_dru.load_weights(os.path.join(self.directory, "model-bw_dru.h5"))
 
-        self.json_file_tkdi = open(os.path.join(self.directory, "model-bw_tkdi.json"), "r")
-        self.model_json_tkdi = self.json_file_tkdi.read()
-        self.json_file_tkdi.close()
-        self.loaded_model_tkdi = model_from_json(self.model_json_tkdi)
-        self.loaded_model_tkdi.load_weights(os.path.join(self.directory, "model-bw_tkdi.h5"))
+        # self.json_file_tkdi = open(os.path.join(self.directory, "model-bw_tkdi.json"), "r")
+        # self.model_json_tkdi = self.json_file_tkdi.read()
+        # self.json_file_tkdi.close()
+        # self.loaded_model_tkdi = model_from_json(self.model_json_tkdi)
+        # self.loaded_model_tkdi.load_weights(os.path.join(self.directory, "model-bw_tkdi.h5"))
 
-        self.json_file_smn = open(os.path.join(self.directory, "model-bw_smn.json"), "r")
-        self.model_json_smn = self.json_file_smn.read()
-        self.json_file_smn.close()
-        self.loaded_model_smn = model_from_json(self.model_json_smn)
-        self.loaded_model_smn.load_weights(os.path.join(self.directory, "model-bw_smn.h5"))
+        # self.json_file_smn = open(os.path.join(self.directory, "model-bw_smn.json"), "r")
+        # self.model_json_smn = self.json_file_smn.read()
+        # self.json_file_smn.close()
+        # self.loaded_model_smn = model_from_json(self.model_json_smn)
+        # self.loaded_model_smn.load_weights(os.path.join(self.directory, "model-bw_smn.h5"))
+        
+        # Load models
+        self.loaded_model = self.load_model("model-bw")
+        self.loaded_model_dru = self.load_model("model-bw_dru")
+        self.loaded_model_tkdi = self.load_model("model-bw_tkdi")
+        self.loaded_model_smn = self.load_model("model-bw_smn")
 
         self.ct = {}
         self.ct['blank'] = 0
@@ -163,12 +169,12 @@ class Application:
         self.panel3 = tk.Label(self.root) # Current Symbol
         self.panel3.place(x = 500,y=640)
         self.T1 = tk.Label(self.root)
-        self.T1.place(x = 10,y = 640)
+        self.T1.place(x = 10,y = 700)
         self.T1.config(text="Character :",font=("Courier",40,"bold"))
         self.panel4 = tk.Label(self.root) # Word
         self.panel4.place(x = 220,y=700)
         self.T2 = tk.Label(self.root)
-        self.T2.place(x = 10,y = 700)
+        self.T2.place(x = 10,y = 760)
         self.T2.config(text ="Word :",font=("Courier",40,"bold"))
         self.panel5 = tk.Label(self.root) # Sentence
         self.panel5.place(x = 350,y=760)
@@ -206,6 +212,15 @@ class Application:
         self.photo="Empty"
         self.video_loop()
 
+    def load_model(self, name):
+        json_path = os.path.join(self.directory, f"{name}.json")
+        weights_path = os.path.join(self.directory, f"{name}.h5")
+        with open(json_path, "r") as json_file:
+            model_json = json_file.read()
+        model = model_from_json(model_json)
+        model.load_weights(weights_path)
+        print(f"{name} model input shape:", model.input_shape)
+        return model
 
     def video_loop(self):
         ok, frame = self.vs.read()
@@ -236,35 +251,41 @@ class Application:
             self.panel5.config(text=self.str,font=("Courier",40))
             predicts=self.hs.candidates(self.word)
             predicts = list(predicts)  # convert to list to use indexing like predicts[0], predicts[1], etc.
-            if(len(predicts) > 0):
-                self.bt1.config(text=predicts[0],font = ("Courier",20))
-            else:
-                self.bt1.config(text="")
-            if(len(predicts) > 1):
-                self.bt2.config(text=predicts[1],font = ("Courier",20))
-            else:
-                self.bt2.config(text="")
-            if(len(predicts) > 2):
-                self.bt3.config(text=predicts[2],font = ("Courier",20))
-            else:
-                self.bt3.config(text="")
-            if(len(predicts) > 3):
-                self.bt4.config(text=predicts[3],font = ("Courier",20))
-            else:
-                self.bt4.config(text="")
-            if(len(predicts) > 4):
-                self.bt4.config(text=predicts[4],font = ("Courier",20))
-            else:
-                self.bt4.config(text="")                
+            # if(len(predicts) > 0):
+            #     self.bt1.config(text=predicts[0],font = ("Courier",20))
+            # else:
+            #     self.bt1.config(text="")
+            # if(len(predicts) > 1):
+            #     self.bt2.config(text=predicts[1],font = ("Courier",20))
+            # else:
+            #     self.bt2.config(text="")
+            # if(len(predicts) > 2):
+            #     self.bt3.config(text=predicts[2],font = ("Courier",20))
+            # else:
+            #     self.bt3.config(text="")
+            # if(len(predicts) > 3):
+            #     self.bt4.config(text=predicts[3],font = ("Courier",20))
+            # else:
+            #     self.bt4.config(text="")
+            # if(len(predicts) > 4):
+            #     self.bt4.config(text=predicts[4],font = ("Courier",20))
+            # else:
+            #     self.bt4.config(text="")   
+            buttons = [self.bt1, self.bt2, self.bt3, self.bt4, self.bt5]
+            for i, btn in enumerate(buttons):
+                btn.config(text=predicts[i] if i < len(predicts) else "", font=("Courier", 20))
+                       
         self.root.after(30, self.video_loop)
+        
     def predict(self,test_image):
-        test_image = cv2.resize(test_image, (128,128))
-        result = self.loaded_model.predict(test_image.reshape(1, 128, 128, 1))
-        result_dru = self.loaded_model_dru.predict(test_image.reshape(1 , 128 , 128 , 1))
-        result_tkdi = self.loaded_model_tkdi.predict(test_image.reshape(1 , 128 , 128 , 1))
-        result_smn = self.loaded_model_smn.predict(test_image.reshape(1 , 128 , 128 , 1))
-        prediction={}
-        prediction['blank'] = result[0][0]
+        # input_shape = self.loaded_model.input_shape[1:3]
+        test_image = cv2.resize(test_image, (128, 128))
+        test_image = test_image.reshape(1, 128, 128, 1)
+        result = self.loaded_model.predict(test_image)
+        result_dru = self.loaded_model_dru.predict(test_image)
+        result_tkdi = self.loaded_model_tkdi.predict(test_image)
+        result_smn = self.loaded_model_smn.predict(test_image)
+        prediction = {'blank': result[0][0]}
         inde = 1
         for i in ascii_uppercase:
             prediction[i] = result[0][inde]
@@ -331,36 +352,41 @@ class Application:
                     self.str = ""
                 self.blank_flag = 0
                 self.word += self.current_symbol
-    def action1(self):
-    	predicts=self.hs.suggest(self.word)
-    	if(len(predicts) > 0):
-            self.word=""
-            self.str+=" "
-            self.str+=predicts[0]
-    def action2(self):
-    	predicts=self.hs.suggest(self.word)
-    	if(len(predicts) > 1):
-            self.word=""
-            self.str+=" "
-            self.str+=predicts[1]
-    def action3(self):
-    	predicts=self.hs.suggest(self.word)
-    	if(len(predicts) > 2):
-            self.word=""
-            self.str+=" "
-            self.str+=predicts[2]
-    def action4(self):
-    	predicts=self.hs.suggest(self.word)
-    	if(len(predicts) > 3):
-            self.word=""
-            self.str+=" "
-            self.str+=predicts[3]
-    def action5(self):
-    	predicts=self.hs.suggest(self.word)
-    	if(len(predicts) > 4):
-            self.word=""
-            self.str+=" "
-            self.str+=predicts[4]
+def action1(self):
+    predicts = self.hs.suggest(self.word)
+    if len(predicts) > 0:
+        self.word = ""
+        self.str += " "
+        self.str += predicts[0]
+
+def action2(self):
+    predicts = self.hs.suggest(self.word)
+    if len(predicts) > 1:
+        self.word = ""
+        self.str += " "
+        self.str += predicts[1]
+
+def action3(self):
+    predicts = self.hs.suggest(self.word)
+    if len(predicts) > 2:
+        self.word = ""
+        self.str += " "
+        self.str += predicts[2]
+
+def action4(self):
+    predicts = self.hs.suggest(self.word)
+    if len(predicts) > 3:
+        self.word = ""
+        self.str += " "
+        self.str += predicts[3]
+
+def action5(self):
+    predicts = self.hs.suggest(self.word)
+    if len(predicts) > 4:
+        self.word = ""
+        self.str += " "
+        self.str += predicts[4]
+
     def destructor(self):
         print("Closing Application...")
         self.root.destroy()
