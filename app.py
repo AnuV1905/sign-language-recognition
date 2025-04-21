8d1e8bdf1183112f72be5e90c136e2f57fdec2f3
Anushka Verma <vermaanushka1905@gmail.com>
2025-04-21 21:20:51 +0530

    removed garbage comments

--- app.py
+++ app.py
@@ -13,102 +13,7 @@ import matplotlib.pyplot as plt
 from spellchecker import SpellChecker
 from string import ascii_uppercase
 
-# class Application:
-#     def __init__(self):
-# 	    self.directory = 'model'
-#         # self.hs = hunspell.HunSpell('en_US.dic', 'en_US.aff')
-#         self.hs = SpellChecker()
-#         self.vs = cv2.VideoCapture(0)
-#         self.current_image = None
-#         self.current_image2 = None
-        
-#         self.json_file = open(os.path.join(self.directory, "model-bw.json", "r")
-#         self.model_json = self.json_file.read()
-#         self.json_file.close()
-#         self.loaded_model = model_from_json(self.model_json)
-#         self.loaded_model.load_weights(os.path.join(self.directory, "model-bw.h5")
 
-#         self.json_file_dru = open(os.path.join(self.directory, "model-bw_dru.json" , "r")
-#         self.model_json_dru = self.json_file_dru.read()
-#         self.json_file_dru.close()
-#         self.loaded_model_dru = model_from_json(self.model_json_dru)
-#         self.loaded_model_dru.load_weights(self.directory + "model-bw_dru.h5")
-
-#         self.json_file_tkdi = open(os.path.join(self.directory, "model-bw_tkdi.json" , "r")
-#         self.model_json_tkdi = self.json_file_tkdi.read()
-#         self.json_file_tkdi.close()
-#         self.loaded_model_tkdi = model_from_json(self.model_json_tkdi)
-#         self.loaded_model_tkdi.load_weights(self.directory + "model-bw_tkdi.h5")
-
-#         self.json_file_smn = open(os.path.join(self.directory, "model-bw_smn.json" , "r")
-#         self.model_json_smn = self.json_file_smn.read()
-#         self.json_file_smn.close()
-#         self.loaded_model_smn = model_from_json(self.model_json_smn)
-#         self.loaded_model_smn.load_weights(self.directory + "model-bw_smn.h5")
-        
-#         self.ct = {}
-#         self.ct['blank'] = 0
-#         self.blank_flag = 0
-#         for i in ascii_uppercase:
-#           self.ct[i] = 0
-#         print("Loaded model from disk")
-#         self.root = tk.Tk()
-#         self.root.title("Sign language to Text Converter")
-#         self.root.protocol('WM_DELETE_WINDOW', self.destructor)
-#         self.root.geometry("900x1100")
-#         self.panel = tk.Label(self.root)
-#         self.panel.place(x = 135, y = 10, width = 640, height = 640)
-#         self.panel2 = tk.Label(self.root) # initialize image panel
-#         self.panel2.place(x = 460, y = 95, width = 310, height = 310)
-        
-#         self.T = tk.Label(self.root)
-#         self.T.place(x=31,y = 17)
-#         self.T.config(text = "Sign Language to Text",font=("courier",40,"bold"))
-#         self.panel3 = tk.Label(self.root) # Current SYmbol
-#         self.panel3.place(x = 500,y=640)
-#         self.T1 = tk.Label(self.root)
-#         self.T1.place(x = 10,y = 640)
-#         self.T1.config(text="Character :",font=("Courier",40,"bold"))
-#         self.panel4 = tk.Label(self.root) # Word
-#         self.panel4.place(x = 220,y=700)
-#         self.T2 = tk.Label(self.root)
-#         self.T2.place(x = 10,y = 700)
-#         self.T2.config(text ="Word :",font=("Courier",40,"bold"))
-#         self.panel5 = tk.Label(self.root) # Sentence
-#         self.panel5.place(x = 350,y=760)
-#         self.T3 = tk.Label(self.root)
-#         self.T3.place(x = 10,y = 760)
-#         self.T3.config(text ="Sentence :",font=("Courier",40,"bold"))
-
-#         self.T4 = tk.Label(self.root)
-#         self.T4.place(x = 250,y = 820)
-#         self.T4.config(text = "Suggestions",fg="red",font = ("Courier",40,"bold"))
-
-#         self.btcall = tk.Button(self.root,command = self.action_call,height = 0,width = 0)
-#         self.btcall.config(text = "About",font = ("Courier",14))
-#         self.btcall.place(x = 825, y = 0)
-
-#         self.bt1=tk.Button(self.root, command=self.action1,height = 0,width = 0)
-#         self.bt1.place(x = 26,y=890)
-#         #self.bt1.grid(padx = 10, pady = 10)
-#         self.bt2=tk.Button(self.root, command=self.action2,height = 0,width = 0)
-#         self.bt2.place(x = 325,y=890)
-#         #self.panel3.place(x = 10,y=660)
-#         # self.bt2.grid(row = 4, column = 1, columnspan = 1, padx = 10, pady = 10, sticky = tk.NW)
-#         self.bt3=tk.Button(self.root, command=self.action3,height = 0,width = 0)
-#         self.bt3.place(x = 625,y=890)
-#         # self.bt3.grid(row = 4, column = 2, columnspan = 1, padx = 10, pady = 10, sticky = tk.NW)
-#         self.bt4=tk.Button(self.root, command=self.action4,height = 0,width = 0)
-#         self.bt4.place(x = 125,y=950)
-#         # self.bt4.grid(row = bt1, column = 0, columnspan = 1, padx = 10, pady = 10, sticky = tk.N)
-#         self.bt5=tk.Button(self.root, command=self.action5,height = 0,width = 0)
-#         self.bt5.place(x = 425,y=950)
-#         # self.bt5.grid(row = 5, column = 1, columnspan = 1, padx = 10, pady = 10, sticky = tk.N)
-#         self.str=""
-#         self.word=""
-#         self.current_symbol="Empty"
-#         self.photo="Empty"
-#         self.video_loop()
 get_custom_objects()['Sequential'] = Sequential
 class Application:
     def __init__(self):
@@ -118,29 +23,7 @@ class Application:
         self.current_image = None
         self.current_image2 = None
         
-        # self.json_file = open(os.path.join(self.directory, "model-bw.json"), "r")
-        # self.model_json = self.json_file.read()
-        # self.json_file.close()
-        # self.loaded_model = model_from_json(self.model_json)
-        # self.loaded_model.load_weights(os.path.join(self.directory, "model-bw.h5"))
-
-        # self.json_file_dru = open(os.path.join(self.directory, "model-bw_dru.json"), "r")
-        # self.model_json_dru = self.json_file_dru.read()
-        # self.json_file_dru.close()
-        # self.loaded_model_dru = model_from_json(self.model_json_dru)
-        # self.loaded_model_dru.load_weights(os.path.join(self.directory, "model-bw_dru.h5"))
-
-        # self.json_file_tkdi = open(os.path.join(self.directory, "model-bw_tkdi.json"), "r")
-        # self.model_json_tkdi = self.json_file_tkdi.read()
-        # self.json_file_tkdi.close()
-        # self.loaded_model_tkdi = model_from_json(self.model_json_tkdi)
-        # self.loaded_model_tkdi.load_weights(os.path.join(self.directory, "model-bw_tkdi.h5"))
-
-        # self.json_file_smn = open(os.path.join(self.directory, "model-bw_smn.json"), "r")
-        # self.model_json_smn = self.json_file_smn.read()
-        # self.json_file_smn.close()
-        # self.loaded_model_smn = model_from_json(self.model_json_smn)
-        # self.loaded_model_smn.load_weights(os.path.join(self.directory, "model-bw_smn.h5"))
+      
         
         # Load models
         self.loaded_model = self.load_model("model-bw")
@@ -251,26 +134,7 @@ class Application:
             self.panel5.config(text=self.str,font=("Courier",40))
             predicts=self.hs.candidates(self.word)
             predicts = list(predicts)  # convert to list to use indexing like predicts[0], predicts[1], etc.
-            # if(len(predicts) > 0):
-            #     self.bt1.config(text=predicts[0],font = ("Courier",20))
-            # else:
-            #     self.bt1.config(text="")
-            # if(len(predicts) > 1):
-            #     self.bt2.config(text=predicts[1],font = ("Courier",20))
-            # else:
-            #     self.bt2.config(text="")
-            # if(len(predicts) > 2):
-            #     self.bt3.config(text=predicts[2],font = ("Courier",20))
-            # else:
-            #     self.bt3.config(text="")
-            # if(len(predicts) > 3):
-            #     self.bt4.config(text=predicts[3],font = ("Courier",20))
-            # else:
-            #     self.bt4.config(text="")
-            # if(len(predicts) > 4):
-            #     self.bt4.config(text=predicts[4],font = ("Courier",20))
-            # else:
-            #     self.bt4.config(text="")   
+            
             buttons = [self.bt1, self.bt2, self.bt3, self.bt4, self.bt5]
             for i, btn in enumerate(buttons):
                 btn.config(text=predicts[i] if i < len(predicts) else "", font=("Courier", 20))
@@ -434,20 +298,9 @@ def action5(self):
         self.tx3 = tk.Label(self.root1)
         self.tx3.place(x = 380,y = 250)
         self.tx3.config(text = "Zeba Khalid\n2202015", font = ("Courier",15,"bold"))
-
-        # self.photo4 = tk.PhotoImage(file='Pictures/sheldon.png')
-        # self.w4 = tk.Label(self.root1, image = self.photo4)
-        # self.w4.place(x = 560, y = 105)
-        # self.tx4 = tk.Label(self.root1)
-        # self.tx4.place(x = 560,y = 250)
-        # self.tx4.config(text = "Sheldon\nIIT2016137", font = ("Courier",15,"bold"))
         
-        # self.photo5 = tk.PhotoImage(file='Pictures/sid.png')
-        # self.w5 = tk.Label(self.root1, image = self.photo5)
-        # self.w5.place(x = 740, y = 105)
-        # self.tx5 = tk.Label(self.root1)
-        # self.tx5.place(x = 740,y = 250)
-        # self.tx5.config(text = "Siddhant\nIIT2016069", font = ("Courier",15,"bold"))
+        
+
         
         self.tx7 = tk.Label(self.root1)
         self.tx7.place(x = 170,y = 360)
