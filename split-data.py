a30098b54f64276384581627d9237e9843bee172
Anushka Verma <vermaanushka1905@gmail.com>
2025-04-23 14:23:10 +0530

    preprocessing done

--- split-data.py
+++ split-data.py
@@ -3,9 +3,9 @@ import shutil
 import random
 
 # CONFIGURATION
-src_base = 'data2/raw/original_images'
+src_base = 'data2/raw'
 dst_base = 'data2'
-train_split = 0.8  # 80% train, 20% test
+train_split = 0.6  # 80% train, 20% test
 delete_raw_after_split = True  # set to False if you want to keep raw images
 
 total_summary = {}
