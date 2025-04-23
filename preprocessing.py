cb16d0136ebcea4be2cabe21e821fdfc73a95d6e
Anushka Verma <vermaanushka1905@gmail.com>
2025-04-23 13:02:51 +0530

    deleted dataset

--- preprocessing.py
+++ preprocessing.py
@@ -4,20 +4,21 @@ import os
 import csv
 from image_processing import func
 
-# Create necessary directories if they don't exist
-if not os.path.exists("data2"):
-    os.makedirs("data2")
-if not os.path.exists("data2/train"):
-    os.makedirs("data2/train")
-if not os.path.exists("data2/test"):
-    os.makedirs("data2/test")
-
-path = "train"
+# if not os.path.exists("data2"):
+#     os.makedirs("data2")
+# if not os.path.exists("data2/train"):
+#     os.makedirs("data2/train")
+# if not os.path.exists("data2/test"):
+#     os.makedirs("data2/test")
+path = "data2/raw"
 path1 = "data2"
 a = ['label']
 
 for i in range(64 * 64):
     a.append("pixel" + str(i))
+
+
+#outputLine = a.tolist()
 
 label = 0
 var = 0
@@ -26,40 +27,33 @@ c2 = 0
 
 for (dirpath, dirnames, filenames) in os.walk(path):
     for dirname in dirnames:
-        print(dirname)
-        for (direcpath, direcnames, files) in os.walk(os.path.join(path, dirname)):
-            train_dir = os.path.join(path1, "train", dirname)
-            test_dir = os.path.join(path1, "test", dirname)
-
-            if not os.path.exists(train_dir):
-                os.makedirs(train_dir)
-            if not os.path.exists(test_dir):
-                os.makedirs(test_dir)
-
-            # For all files, process and write to train/test
-            num = 100000000000000000  # Artificially large to ensure all go to train
+        print(f"Processing directory: {dirname}")
+        for (direcpath, direcnames, files) in os.walk(path + "/" + dirname):
+            if not os.path.exists(path1 + "/raw/" + dirname):
+                os.makedirs(path1 + "/raw/" + dirname)
+            # if not os.path.exists(path1 + "/test/" + dirname):
+            #     os.makedirs(path1 + "/test/" + dirname)
+            # num = 0.75*len(files)
+            num = 100000000000000000
             i = 0
             for file in files:
                 var += 1
-                actual_path = os.path.join(path, dirname, file)
-                actual_path1 = os.path.join(train_dir, file)
-                actual_path2 = os.path.join(test_dir, file)
-
+                actual_path = path + "/" + dirname + "/" + file
+                actual_path1 = path1 + "/" + "raw" + "/" + dirname + "/" + file
+                # actual_path2 = path1 + "/" + "test" + "/" + dirname + "/" + file
                 img = cv2.imread(actual_path, 0)
                 bw_image = func(actual_path)
                 bw_image = cv2.resize(bw_image, (128, 128))
-
                 if i < num:
                     c1 += 1
                     cv2.imwrite(actual_path1, bw_image)
-                else:
-                    c2 += 1
-                    cv2.imwrite(actual_path2, bw_image)
+                # else:
+                #     c2 += 1
+                #     cv2.imwrite(actual_path2, bw_image)
 
-                i += 1
+                i = i + 1
 
-        label += 1
-
-print("Total Images Processed:", var)
-print("Training Images:", c1)
-print("Testing Images:", c2)
+        label = label + 1
+print(var)
+print(c1)
+print(c2)
